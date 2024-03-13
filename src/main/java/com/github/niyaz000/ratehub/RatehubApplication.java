9f3d7c23c825492dc27ce264baa457df99009660
Niyaz K <niyazk2991@gmail.com>
2024-03-13 22:50:01 +0530

    Add initial project setup

---
+++ src/main/java/com/github/niyaz000/ratehub/RatehubApplication.java
@@ -0,0 +1,13 @@
+package com.github.niyaz000.ratehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/niyaz000/ratehub/RatehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.niyaz000.ratehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
